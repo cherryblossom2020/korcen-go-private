89763a05aefb633951f5b2bcc420b8378d1021a3
shibaisdog <kimjaehoon5142@gmail.com>
2025-01-12 01:28:29 +0900

    add example

---
+++ example/example.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fluffy-melli/korcen-go"
+)
+
+func main() {
+	fmt.Println(korcen.Check(""))
+}
---
+++ example/go.mod
@@ -0,0 +1,5 @@
+module example
+
+go 1.22.2
+
+require github.com/fluffy-melli/korcen-go v0.1.0
