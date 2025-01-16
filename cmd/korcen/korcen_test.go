2e4b71d0bf9bdbecb449fef8e8eb74ef2abe0818
shibaisdog <kimjaehoon5142@gmail.com>
2025-01-16 17:43:30 +0900

    Setup go.yml

--- cmd/korcen/korcen_test.go
+++ cmd/korcen/korcen_test.go
@@ -3,15 +3,17 @@ package korcen
 import (
 	"fmt"
 	"testing"
+
+	"github.com/fluffy-melli/korcen-go"
 )
 
 func Benchmark0(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		Check("")
+		korcen.Check("")
 	}
 }
 
 func Test0(t *testing.T) {
-	fmt.Println(Check(""))
+	fmt.Println(korcen.Check(""))
 }
